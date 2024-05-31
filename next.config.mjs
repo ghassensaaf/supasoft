367a2527fbf229fe922e79920296f6dc4a5aa98c
Ghassen Saaf <saafghassen@gmail.com>
2024-05-31 19:25:29 +0100

    refactor: clean project

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,3 @@
-import { withContentlayer } from "next-contentlayer";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -8,4 +7,4 @@ const nextConfig = {
 	},
 };
 
-export default withContentlayer(nextConfig);
+export default nextConfig;
